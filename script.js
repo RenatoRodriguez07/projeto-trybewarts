c13738077f4ceb09887c40f2b9afff3a0cf9326d
Renato Rodriguez <renatorofrigue07@gmail.com>
2025-06-18 21:41:39 -0300

    feat(ui) completando formulario e ajustando responsividade para mobile

--- script.js
+++ script.js
@@ -12,3 +12,55 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+const checkbox = document.getElementById('agreement');
+const submitBtn = document.getElementById('submit-btn');
+
+checkbox.addEventListener('change', function () {
+  submitBtn.disabled = !checkbox.checked;
+});
+
+const textarea = document.getElementById('textarea');
+const counter = document.getElementById('counter');
+const maxChars = 500;
+
+textarea.addEventListener('input', function () {
+  const remaining = maxChars - textarea.value.length;
+  counter.textContent = remaining;
+});
+
+document
+  .getElementById('evaluation-form')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById('input-name').value;
+    const lastName = document.getElementById('input-lastname').value;
+    const email = document.getElementById('input-email').value;
+    const house = document.getElementById('house').value;
+    const family =
+      document.querySelector('input[name="family"]:checked')?.value || '';
+    const materias = Array.from(
+      document.querySelectorAll('input.subject:checked')
+    )
+      .map((el) => el.value)
+      .join(', ');
+    const avaluation =
+      document.querySelector('input[name="rate"]:checked')?.value || '';
+    const observations = document.getElementById('textarea').value;
+
+    const formDataHTML = `
+    <p>Nome: ${name} ${lastName}</p>
+    <p>Email: ${email}</p>
+    <p>Casa: ${house}</p>
+    <p>Família: ${family}</p>
+    <p>Matérias: ${materias}</p>
+    <p>Avaliação: ${avaluation}</p>
+    <p>Observações: ${observations}</p>
+    `;
+
+    document.getElementById('evaluation-form').style.display = 'none';
+    const formDataDiv = document.getElementById('form-data');
+    formDataDiv.innerHTML = formDataHTML;
+    formDataDiv.style.display = 'block';
+  });
